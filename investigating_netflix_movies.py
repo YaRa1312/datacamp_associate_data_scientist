25a097de62a164b3d51c91b5454653dd995f0a67
Iryna Pleshyvtseva <96065857+YaRa1312@users.noreply.github.com>
2024-09-05 21:25:15 +0200

    removed the extra code and left the solutions only

--- investigating_netflix_movies.py
+++ investigating_netflix_movies.py
@@ -32,59 +32,17 @@ netflix_df = pd.read_csv("netflix_data.csv")
 # Start coding here! Use as many cells as you like
 # the most frequent movie duration in the 1990s
 
-# for duration in netflix_df['duration']:
-#     all_durations.append(duration)
-
-# series = pd.Series(all_durations)
-
-# duration = series.value_counts().idxmax()
-
-# all_1990s_movies_with_durations
-
-##################################################################################################################################################3
-# all_1990s_movies_durations = []
-
-# for index, row in netflix_df.iterrows():
-#     if row['release_year'] >= 1990 and row['release_year'] < 2000 and row['type'] == 'Movie':
-#         all_1990s_movies_durations.append(row['duration'])
-            
-# series = pd.Series(all_1990s_movies_durations)
-
-# duration = series.value_counts().idxmax()
-##################################################################################################################################################
-
-# print(duration)
-
 all_1990_movies_durations = netflix_df[(netflix_df['release_year'] >= 1990) & (netflix_df['release_year'] <= 1999) & (netflix_df['type'] == 'Movie')]['duration'].to_list()
 series = pd.Series(all_1990_movies_durations)
 duration = series.value_counts().idxmax()
 print(duration)
 
 # the number of short action movies
-# short_movies = []
-
-# for duration in all_durations:
-#     if duration < 90:
-#         short_movies.append(duration)
-
-# short_movie_count = len(short_movies)
-
-# netflix_df[netflix_df['genre'] == 'action']
 
 short_movie_count = 0
 
 for label, row in netflix_df.iterrows():
     if row['release_year'] >= 1990 and row['release_year'] < 2000 and row['type'] == 'Movie' and row['genre'] == 'Action' and row['duration'] < 90:
         short_movie_count += 1
-
-# short_movie_count = len(netflix_df[
-#     (netflix_df['release_year'] >= 1990) &
-#     (netflix_df['release_year'] < 2000) &
-#     (netflix_df['type'] == 'Movie') &
-#     (netflix_df['genre'] == 'action') &
-#     (netflix_df['duration'] < 90)
-# ])
-
+                
 print(short_movie_count)
-
-# short_movie_count = []
